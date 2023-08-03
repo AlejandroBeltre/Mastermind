d71da8758720e6524c9e808dccaadab63ea67b68
Alejandro <1115242@est.intec.edu.do>
2023-08-03 18:36:48 -0400

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,24 @@
+/*
+    Enunciado: Mastermind.
+    Integrantes:
+                1115242 - Alejandro Aquiles Beltre Pimentel
+                1113810 - Henry Hermes Alburquerque Baez
+    Fecha: 03-08-2023
+ */
+
+
+#include <iostream>
+#include <>
+
+using namespace std;
+
+int main() {
+    int numero[4] = {4, 1, 2, 6};
+    char intentosNumeros[4];
+
+    for(int i = 10; i = 0; i--) {
+        cout << "Introduzca los cuatros numeros" << endl;
+        cin >> numeros << endl;
+    }
+    return 0;
+}
