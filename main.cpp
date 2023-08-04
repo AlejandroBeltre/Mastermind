7f327c9ed80a0b09202fc37cccf1bf957e154d09
Alejandro <1115242@est.intec.edu.do>
2023-08-03 20:08:27 -0400

    Mayoria de cambios falta debuggear el programa t validar entrada.

--- main.cpp
+++ main.cpp
@@ -12,24 +12,51 @@
 
 #include <iostream>
 #include <string>
+#include <cstdlib>
 
 using namespace std;
 
 int main() {
     int numeroAdivinar[4] = {4, 1, 2, 6};
     string numeros;
+    int fallos = 0;
 
     for(int i = 10; i > 0; i--) {
+        cout << "-------------------------------" << endl;
         cout << "Introduzca los cuatros numeros: " << endl;
         cin >> numeros;
+        int aciertos = 0;
+        cout << i << ". ";
         for (int j = 0; j < 4; j++) {
-            if (numeroAdivinar[j] == isdigit(numeros[j])) {
-                cout << "C" << endl;
+            if (numeros[j] - '0' == numeroAdivinar[j]) {
+                cout << "C";
+                aciertos++;
             }
-            else (){
-                cout << "F" << endl;
+            else{
+                aciertos = 0;
+                for(int k = 0; k < 4; k++){
+                    if(numeros[j] - '0' == numeroAdivinar[k]) {
+                        cout << "F";
+                        break;
+                    }
+                    else{
+                        fallos ++;
+                    }
+                    if (fallos == 4){
+                        cout << "X";
+                    }
+                }
+                fallos = 0;
             }
         }
+        if(aciertos == 4){
+            cout << "" << endl;
+            cout << "Felicitaciones, usted ha ganado." << endl;
+            cout << "Su puntaje es: " << i * 10 << endl;
+            break;
+        }
+        cout << "" << endl;
+        cout << "Tiene " << i - 1 << " intentos restantes." << endl;
     }
     return 0;
 }
